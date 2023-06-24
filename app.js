1ab4747edaa5ab1741f000ecc46b87d2f7ef571b
Anthonie Derryanto <mac@macs-MacBook-Pro.local>
2023-06-24 22:34:46 +0700

    Making Checkout Js

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
+
+
+// Details Order JS Start
 let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick =() =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-let slides = document.querySelectorAll('.slide-container');
-let index = 0;
-
-function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
-}
-
-function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
-}
 
 document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
     image_1.addEventListener('click', () =>{
@@ -41,4 +23,58 @@ document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
         document.querySelector('.big-image-3').src = src;
     });
 });
+// Details Order JS End
+
+
+
+// Checkout JS Start
+const user = {
+    name : 'Ksatria Pamungkas',
+    number : '08123456789',
+    order : 'Monas',
+    address : 'Sabang'
+}
+
+
+const nameElm = document.getElementById('name')
+const numberElm = document.getElementById('number')
+const ordersElm = document.getElementById('orders')
+const extraOrdersElm = document.getElementById('extraOrders')
+const ticketsElm = document.getElementById('tickets')
+const timeElm = document.getElementById('time')
+const addressElm = document.getElementById('address')
+const messageElm = document.getElementById('message')
+
+const btn_orderElm = document.querySelector('.btn')
+
+
+
+btn_orderElm = addEventListener("click", ()=>{
+    if((nameElm.value.length > 0 && numberElm.value.length > 0 && ordersElm.value.length > 0 && addressElm.value.length > 0) &&
+        (extraOrdersElm.value.length > 0 || ticketsElm.value.length > 0 || timeElm.value.length > 0 || messageElm.value.length > 0)){
+
+            if(nameElm.value == user.name && numberElm.value == user.number && ordersElm.value == user.order && addressElm.value == user.address ){
+                alert('Pemesanan Anda berhasil')
+            }else{
+                alert('Data yang harus diisi harus sesuai dengan data akun yang terdaftar dan sesuai dengan pemesanan')
+            }
+
+        
+    }
+    // else{
+    //     alert('Form harus diisi dengan lengkap')
+    // }
+})
+
+
+
+
+
+
+
+
+// Checkout JS End
+
+
+
 
